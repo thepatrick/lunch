8970ab93e2347df8d73f7f35bde60821e3238207
Patrick Quinn-Graham <make-contact@pftqg.com>
2017-09-03 10:21:45 +1000

    Use now ui kit

--- client/components/PlaceEditRoot.jsx
+++ client/components/PlaceEditRoot.jsx
@@ -8,21 +8,9 @@ import DeletePlaceContainer from '../containers/DeletePlaceContainer';
 function PlaceEditRoot({ params }) {
   return (
     <div className="row">
-      <nav className="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
-        <ul className="nav nav-pills flex-column">
-          <li className="nav-item">
-            <Link className="nav-link" to="/manage">
-              Places
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-        <h1>
-          Place
-          <DeletePlaceContainer placeId={params.id}>Delete</DeletePlaceContainer>
-        </h1>
+      <main className="col-sm-12 col-md-12 pt-5">
+        <DeletePlaceContainer placeId={params.id}>Delete</DeletePlaceContainer>
+        <h1>Place</h1>
         <div className="table-responsive">
           <EditPlace placeId={params.id} />
           <ViewPlace placeId={params.id} />
